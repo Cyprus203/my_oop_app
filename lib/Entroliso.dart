aac50783ece3fee93150949147192eab7f25f075
Cyprus203 <147189660+Cyprus203@users.noreply.github.com>
2025-08-13 16:33:41 +0800

    Create Entroliso.dart

---
+++ lib/Entroliso.dart
@@ -0,0 +1,10 @@
+ class Person {
+
+String name;Entroliso
+int age;19
+  
+     Person(this.Entroliso, this.19);
+     void sayHello() {
+    print('Hello! I am $name and I am $age years old.');
+    }
+}
