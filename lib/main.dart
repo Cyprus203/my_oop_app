5c9a4f60ebf34f23bd6a73749185012710a5bdb7
sancyqt <greggytzy2005@gmail.com>
2025-08-15 13:50:24 +0800

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'Entroliso.dart';
 import 'Arrogante.dart';
 import 'Sienes.dart';
+import 'Carbero.dart';
 
 void main() {
   var person1 = Person('Entroliso', 19);
   var person2 = Person('Arrogante', 19);
   var person3 = Person('Sienes', 19);
+  var person3 = Person('Carbero', 19);
   
   person1.sayHello(); // Output: Hello! I am Entroliso and I am 19 years old.
   person2.sayHello(); // Output: Hello! I am Arrogante and I am 19 years old.
   person3.sayHello(); // Output: Hello! I am Sienes and I am 19 years old.
+  person4.sayHello(); // Output: Hello! I am Carbero and I am 19 years old.
